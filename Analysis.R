9b7a3522799abc3654f25c1018586ca44bb78a6f
Ben Rogers <bjroger@ku.edu>
2016-06-01 15:19:37 -0500

    Added analysis file, and Index to Data

---
+++ Analysis.R
@@ -0,0 +1,6 @@
+## We'll store the analyses of the data we collect in here
+loc <- "/Users/bjr/KUDropbox/Dropbox/LGBT Interest group data/"
+
+dat <- read.csv(paste0(loc,"JT DHM LGBT Group Resources.csv"))
+
+head(dat)
