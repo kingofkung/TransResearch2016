2f05e6666dfb85c84973cf2f43a817249e442922
kingofkung <bjroger@ku.edu>
2016-06-06 15:41:03 -0500

    rearrange open files

--- HRCCombine.R
+++ HRCCombine.R
@@ -7,8 +7,4 @@ head(HRC)
 HRC$State[46]
 
 
-HRC$abb <- unlist(lapply(HRC$State, function(u) state.abb[match(u, state.name)]))
 
-HRC$Codes <- paste0(HRC$abb, HRC$Year)
-
-popnprice$matchcodes <- paste0(popnprice$state, popnprice$bmfyear)
--- OpenFiles.R
+++ OpenFiles.R
@@ -10,36 +10,9 @@ cleaneddata <- read.dta13(paste0(loc, "jami and don lgbt data cleaned 5.20.16.dt
 lgbtallcoll <- read.dta13(paste0(loc, "jami and don all lgbt group collapse 5.21.16.dta"))
 lgbtcollno234 <- read.dta13(paste0(loc, "jami and don lgbt group collapse no 2 3 4 groups  5.21.16.dta"))
 
-## read in taylor lgbt measure
-
-taylorlgbt <- read.dta13(paste0(loc, "taylor lgbt measure 3.20.14.dta"))
-
-## read in pop/price index
-popnprice <- read.dta13(paste0(loc, "pop and priceindex.dta"))
-
-## read in HRC's policy index
-HRC <- read.csv(paste0(loc, "Index2015.csv"))
-
-## read in state public opinion data
-stateopp <- read.xlsx(paste0(loc, "StateOpp.xlsx"), 1)
-
-
-## So basically we want to add a column that has the state as either a
-## name in the collapse files or an abbreviation in the taylor file.
-taylorlgbt$stateabb <- NA
-taylorlgbt$stateabb <- unlist(lapply(taylorlgbt$statename, function(x){
-    state.abb[match(x, state.name)]
-    }
-
-))
-
-## Appears to have worked exactly as intended
-## table(taylorlgbt$stateabb, taylorlgbt$statename)
-
 ## So what do you say we create some codes with the correct naming
 ## conventions? Now that we have given taylorlgbt state abbreviations,
 ## we should be able to make codes to do some matching.
-taylorlgbt$matchcode <- paste0(taylorlgbt$stateabb, taylorlgbt$year)
 lgbtallcoll$matchcode <- paste0(lgbtallcoll$state, lgbtallcoll$bmfyear)
 lgbtcollno234$matchcode <- paste0(lgbtcollno234$state, lgbtcollno234$bmfyear)
 
@@ -48,3 +21,39 @@ lgbtcollno234$matchcode <- paste0(lgbtcollno234$state, lgbtcollno234$bmfyear)
 
 colnames(lgbtallcoll) <- paste0(colnames(lgbtallcoll), "all")
 colnames(lgbtcollno234) <- paste0(colnames(lgbtcollno234), "no234")
+
+
+
+## read in taylor lgbt measure
+
+taylorlgbt <- read.dta13(paste0(loc, "taylor lgbt measure 3.20.14.dta"))
+
+
+## So basically we want to add a column that has the state as either a
+## name in the collapse files or an abbreviation in the taylor file.
+taylorlgbt$stateabb <- NA
+taylorlgbt$stateabb <- unlist(lapply(taylorlgbt$statename, function(x){state.abb[match(x, state.name)]}))
+## Appears to have worked exactly as intended
+## table(taylorlgbt$stateabb, taylorlgbt$statename)
+
+
+
+taylorlgbt$matchcode <- paste0(taylorlgbt$stateabb, taylorlgbt$year)
+
+
+## read in pop/price index
+popnprice <- read.dta13(paste0(loc, "pop and priceindex.dta"))
+popnprice$matchcodes <- paste0(popnprice$state, popnprice$bmfyear)
+
+
+## read in HRC's policy index
+HRC <- read.csv(paste0(loc, "Index2015.csv"))
+HRC$abb <- unlist(lapply(HRC$State, function(u) state.abb[match(u, state.name)]))
+HRC$matchcodes <- paste0(HRC$abb, HRC$Year)
+
+
+## read in state public opinion data
+stateopp <- read.xlsx(paste0(loc, "StateOpp.xlsx"), 1)
+
+
+
