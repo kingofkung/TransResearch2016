c4190f3e75a6afcc280e6e56e67416ab81fb3e5c
kingofkung <bjroger@ku.edu>
2016-06-08 13:05:59 -0500

    Fixed input file path

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,8 @@
 ## We'll store the analyses of the data we collect in here
-loc <- "/Users/bjr/KUDropbox/Dropbox/LGBT Interest group data/"
+loc <- "/Users/bjr/Dropbox/LGBT Interest group data/"
 
 dat <- read.csv(paste0(loc,"JT DHM LGBT Group Resources.csv"))
 
 head(dat)
+
+## Have read in data, will conduct some analyses now
