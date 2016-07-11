6c99fd78b4bd3436ebe67d268cf2b3d7af53763a
kingofkung <bjroger@ku.edu>
2016-07-11 14:35:58 -0500

    Saved new latex output to files

--- Analysis.R
+++ Analysis.R
@@ -1,24 +1,34 @@
 ## We'll store the analyses of the data we collect in here
 library(plyr)
 library(MASS)
-library(zoo)
 library(texreg)
-library(stargazer)
 library(rockchalk)
+
+##' create custom glm function suitable for bulk regression orders
+##' @title custom glm
+##' @param x a vector containing the independent variables that we want to consider
+##' @param deev A character version of our dv of choice.
+##' @return a glm model with deev ~ x, where x is a the of variables of interest
+##' @author Benjamin Rogers
+customglm <- function(x, deev){
+    x <- ifelse(length(x) > 1, paste0(x, collapse = "+"), x)
+    u <- as.formula(paste(deev, x, sep = " ~ "))
+    eval(bquote(glm(.(u), family = "binomial", data = dat)))
+    }
+
 
 loc <- "/Users/bjr/Dropbox/LGBT Interest group data/"
 outlocgit <- "/Users/bjr/GitHub/TransResearch2016/Output/"
+outlocdb <- "/Users/bjr/Dropbox/LGBT Interest group data/"
+
 dat <- read.csv(paste0(loc,"JT DHM LGBT Group Resources.csv"))
 
-head(dat)
-colnames(dat)
 ## Have read in data, will conduct some analyses now
 
 ## lgbtrevpercapita is Dr. Taylor's measure
 
 ## correlations between IVs, dvs
 ## make a file with output
-
 
 realhrccols <- colnames(dat)[grepl("hrc", colnames(dat))]
 laxPhillips <- colnames(dat)[grepl("LP", colnames(dat))]
@@ -29,8 +39,11 @@ deveesofint <- c("doma", "superdoma", "trans_dis", 'gay_disc')
 allcols <- colnames(dat)[grepl("all", colnames(dat))]
 no234cols <- colnames(dat)[grepl("no234", colnames(dat))]
 
-##Typical control variables
-## Note, I had  "inst6014_nom" in here, but decided to remove it as it appeared to be multicollinear with both our ivs of interest as well as the other institutional variable. We can put it back in if we'd like later though
+## Here are some typical control variables. Note, I had "inst6014_nom"
+## in here, but decided to remove it as it appeared to be
+## multicollinear with both our ivs of interest as well as the other
+## institutional variable. We can put it back in if we'd like later
+## though.
 typcont <- c("citi6013", "inst6013_adacope", laxPhillips[9], "ssph", "Williams", "squire", "evangelical")
 typcont <- lapply(seq_along(typcont), function(x) typcont[1:x])
 
@@ -42,21 +55,21 @@ assetVars <- grep("asset|ast", colnames(dat), value = T)
 incasstrevVars <- c(revVars, incVars, assetVars)
 
 ## Do a correlation matrix on the variables of interest
-incContsCors <- cor(dat[, unique(unlist(c(typcont, incasstrevVars))) ], use = "pairwise.complete.obs")
-## write.csv(incContsCors, file = paste0(outlocgit, 'ControlsAndGroupresourcescorrelations.csv'))
+incContsCors <- cor(dat[, unique(unlist(c(deveesofint, typcont, incasstrevVars))) ], use = "pairwise.complete.obs")
+write.csv(incContsCors, file = paste0(outlocdb, 'ControlsAndGroupresourcescorrelations.csv'))
 
-oc,"JT DHM LGBT Group Resources.csv"), row.names = F)
 
 ## Get ScoreCardCats into the correct Order
- SCCLvls <- c("High Priority To Achieve Basic Equality", "Building Equality", "Solidifying Equality", "Working Toward Innovative Equality")
+SCCLvls <- c("High Priority To Achieve Basic Equality", "Building Equality", "Solidifying Equality", "Working Toward Innovative Equality")
 dat$ScoreCardCats <- factor(dat$ScoreCardCats, levels = SCCLvls)
-levels(dat$ScoreCardCats)
+## levels(dat$ScoreCardCats)
 
 SCCdat <- dat[ !is.na(dat$ScoreCardCats),]
-head(SCCdat)
+## head(SCCdat)
 
+## Find out which columns in SCCdat are entirely composed of na's
 nacols <- apply(SCCdat, 2, function(x) all(is.na(x)))
-names(nacols[!nacols==T])
+## names(nacols[!nacols==T])
 
 ## Models for ScoreCardCats ordinal Logit regression
 ## Incomeall isn't working correctly, but its square root and percentages return a result
@@ -65,7 +78,10 @@ SCCdat$iaplus <- SCCdat$incomeall + 1
 SCCdat$iaperc <- SCCdat$incomeall/sum(as.numeric(SCCdat$incomeall))
 incomeall <- as.numeric(SCCdat$incomeall)
 
+## Create the IVs we'd like
 ivls <- c("MeanOppLP", "inst6013_adacope", "inst6014_nom", "citi6013", "iaperc", "realincpercapall", "orgcountall", laxPhillips[2:8])
+
+## And apply across all single function levels
 onevarHRCmods <- lapply(ivls, function(x) {
     r <- as.formula(paste0("ScoreCardCats ~ ",x))
     eval(bquote(polr(.(r), data = SCCdat, method = "logistic")))
@@ -83,18 +99,12 @@ write.table(latextext1 , file = paste0(outlocgit, "HRCmlist.tex"), quote = F, ro
 
 dv1 <- "trans_dis"
 
-customglm <- function(x, deev = dv1){
-    x <- ifelse(length(x) > 1, paste0(x, collapse = "+"), x)
-    u <- as.formula(paste(deev, x, sep = " ~ "))
-    eval(bquote(glm(.(u), family = "binomial", data = dat)))
-    }
 
+## Bind control variables we want to the variables we want to consider in a variable called toreg.
+## Note to self, it appears that we get what we want if we list the variable of interest last rather than first.
+toreg <- lapply(incasstrevVars, function(x) c(typcont[[length(typcont)]], x) )
 
-
-## bind control variables we want to the variables we want to consider in a variable called toreg
-toreg <- lapply(incasstrevVars, function(x) c(x, typcont[[length(typcont)]]) )
-
-tdregsNconts <- lapply(toreg, customglm)
+tdregsNconts <- lapply(toreg, customglm, deev = dv1)
 ## outreg(tdregsNconts, "html")
 
 tdmodsub1 <- tdregsNconts[1:10]
@@ -106,7 +116,7 @@ tdcap1 <- "Event History Analysis of Transgender Discrimination Policy with Cont
 tdlatex1 <- texreg(tdmodsub1, caption.above = T, caption = tdcap1)
 
 tdcap2 <- paste0("Event History Analysis of Transgender Discrimination Policy with Controls, Models 11-", length(tdregsNconts))
-tdmodnames <- paste("model", 11:length(tdregsNconts))
+tdmodnames <- paste("Model", 11:length(tdregsNconts))
 tdlatex2 <- texreg(tdmodsub2, caption.above = T, caption = tdcap2, custom.model.names = tdmodnames)
 
 ## write the results to tables
@@ -114,7 +124,7 @@ write.table(tdlatex1, file = paste0(outlocgit, 'tdmodsub1.txt'), quote = F, row.
 write.table(tdlatex2, file = paste0(outlocgit, 'tdmodsub2.txt'), quote = F, row.names = F, col.names = F)
 
 
-
+## Begin working on gay discrimination variables
 dv2 <- "gay_disc"
 gdmods <- lapply(toreg, customglm, deev = dv2)
 ## outreg(gdmods, 'html')
@@ -122,30 +132,37 @@ gdmods <- lapply(toreg, customglm, deev = dv2)
 ## outreg(gdmods, type = 'html')
 ## Write gay discrimination mods to latex tables
 gdmodsub1 <- gdmods[1:10]
-gdcoefOrder1 <- c(1, 3:(length(typcont) + 2), 2, (length(typcont) + 3):(length(gdmodsub1) + 5))
-
-
 gdmodsub2 <- gdmods[11:length(gdmods)]
 names(gdmodsub2) <- paste("Model", 11:length(gdmods))
-gdcoefOrder2 <- c(1, 3:(length(typcont) + 2), 2, (length(typcont) + 3):(length(gdmodsub1) + 4))
 
 ## outreg(gdmodsub1, type = 'html')
-gdlatex1 <- texreg(gdmodsub1, caption.above = T,
-                   caption = "Event History Analysis of Gay Discrimination Policy with added Controls, 1-10",
-                   reorder.coef = gdcoefOrder1)
+gdlatex1 <- texreg(gdmodsub1, caption.above = T, caption = "Event History Analysis of Gay Discrimination Policy with added Controls, 1-10")
 
-write.table(gdlatex1, file = paste0(outlocgit, "gdlatex1.txt"), quote = F, row.names = F, col.names = F)
 
 modcap2 <- paste0("Event History Analysis of Gay Discrimination Policy with added Controls, 11-", length(gdmods))
-gdlatex2 <- texreg(gdmodsub2, caption.above = T, caption = modcap2, reorder.coef = gdcoefOrder2, custom.model.names = paste("Model", 11:length(gdmods))
+gdlatex2 <- texreg(gdmodsub2, caption.above = T, caption = modcap2, custom.model.names = paste("Model", 11:length(gdmods)))
 
 
-
+write.table(gdlatex1, file = paste0(outlocgit, "gdlatex1.txt"), quote = F, row.names = F, col.names = F)
 write.table(gdlatex2, file = paste0(outlocgit, "gdlatex2.txt"), quote = F, row.names = F, col.names = F)
 
+# some isolated varsofint
+dotsym <- "\\bullet"
 
-m <- glm(gay_disc ~ citi6013 + inst6014_nom  + MeanOppLP + ssph + realastpercap_smallno234, data = dat, "binomial")
-summary(m)
+smallno234ivs <- lapply(typcont, c, "realastpercap_smallno234")
+no234mods <- lapply(smallno234ivs, customglm, deev = "trans_dis")
+no234tex <- texreg(no234mods, caption.above = T, caption = "A further examination of real assets per capita on Transgender Anti-discrimination, sans certain groups", stars = c(.001, .01, .05, .1), symbol = dotsym)
+write.table(no234tex, paste0(outlocgit, "no234textrans.txt"), quote = F, row.names = F, col.names = F)
 
-malt <- glm(gay_disc ~ citi6013  + inst6014_nom   + realastpercap_smallno234 + evangelical + Williams + squire + ssph, data = dat, "binomial")
-summary(malt)
+no234mods <- lapply(smallno234ivs, customglm, deev = "gay_disc")
+no234tex <- texreg(no234mods, caption.above = T, caption = "A further examination of real assets per capita on Gay Anti-discrimination, sans certain groups", stars = c(.001, .01, .05, .1), symbol = dotsym)
+write.table(no234tex, paste0(outlocgit, "no234texgay.txt"), quote = F, row.names = F, col.names = F)
+
+realastivs <- lapply(typcont, c, "realastpercapall")
+for(i in c("gay_disc", "trans_dis")[1:2]){
+    mods <- lapply(realastivs, customglm, deev = i)
+    ifelse(i == "trans_dis", mcap <- "An Examination of Real Assets Per Capita on Transgender Anti-discrimination", mcap <- "An Examination of Real Assets Per Capita on Gay Anti-discrimination")
+    mtex <- texreg(mods, caption.above = T, caption = mcap, stars = c(.001, .01, .05, .1), symbol = dotsym)
+    write.table(mtex, paste0(outlocgit, "realastpercapall",i, ".txt"), quote = F, row.names = F, col.names = F)
+}
+## lapply(mods, summary)
